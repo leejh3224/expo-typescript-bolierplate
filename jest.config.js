67bdaee388d2c8c6306248b099d41c4e58668c90
leejh3224 <leejh3224@naver.com>
2019-01-14 00:57:24 +0900

    update dependencies change testing library from enzyme to react-native-testing-library

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
 	preset: 'jest-expo',
 	moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
 	testMatch: ['<rootDir>src/**/*.test.ts?(x)'],
-	setupTestFrameworkScriptFile: '<rootDir>setup-tests.js',
 }
--- src/Main.test.tsx
+++ src/Main.test.tsx
@@ -1,18 +1,13 @@
-import { shallow } from "enzyme";
 import Main from "Main";
 import * as React from "react";
-import { Text } from "react-native";
+import { render } from "react-native-testing-library";
 
 describe("test", () => {
   it("renders text correctly", () => {
-    const wrapper = shallow(<Main />);
-    const text = wrapper.find(Text);
+    const { getByText } = render(<Main />);
 
-    /**
-     * To check text of Text component, you can reference .prop(children)
-     */
-    expect(text.prop("children")).toBe(
-      "Open up App.js to start working on your app!"
-    );
+    const welcome = "Open up App.js to start working on your app!";
+
+    expect(getByText(welcome).props.children).toBe(welcome);
   });
 });
